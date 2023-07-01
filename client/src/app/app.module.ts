fc4978ae3b85a1a9bd689d55a170a73cfcb2dfbd
Mert Gökmen <mertgkmeen@gmail.com>
2023-07-01 11:53:35 +0300

    added header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { Login2Component } from './components/login2/login2.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, Login2Component, HomeComponent],
+  declarations: [AppComponent, LoginComponent, Login2Component, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
